113520dcbaf382b25bb4ded11be656b5721ffaa4
조민혁 <chohyuk0725@gmail.com>
2022-10-04 04:55:33 +0900

    beforeEach 함수 사용시 타입문제 해결

--- src/test/unit/products.test.ts
+++ src/test/unit/products.test.ts
@@ -8,21 +8,27 @@ const productModel = Product;
 
 productModel.create = jest.fn();
 
+let req: Request | undefined;
+let res: Response | undefined;
+let next: null | NextFunction;
+
 beforeEach(() => {
-  let req = httpmocks.createRequest();
-  let res = httpmocks.createResponse();
-  let next = null;
+  req = httpmocks.createRequest();
+  res = httpmocks.createResponse();
+  next = null;
 });
 
 describe("Product Controller create ", () => {
   beforeEach(() => {
-    req.body = newProduct;
+    if (req) req.body = newProduct;
   });
+
   it("should hava a createProduct function", () => {
     expect(typeof productController.createProduct).toBe("function");
   });
+
   it("should call ProductModel.create", () => {
-    productController.createProduct(req, res, next);
+    productController.createProduct(req as Request, res as Response, next);
     expect(productModel.create).toBeCalledWith(newProduct);
   });
 });
